6b97eb08743da6ad246a36e01eade6fca887bb88
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-05-21 22:00:27 +0700

    Add public path in main config file

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,12 @@
+"use strict";
+
+const path = require("path");
+
 const config = {
   database: "mongodb://localhost/storyline",
   secret: "secret",
-  port: 8000
+  port: 8000,
+  publicPath: path.resolve("public")
 };
 
 module.exports = config;
